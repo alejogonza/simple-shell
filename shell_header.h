32cef96860af20c7d86472b40304b0611906af93
dilanrinconhbtn <937@holbertonschool.com>
2019-08-21 21:54:07 +0000

    [feature] updating header

--- shell_header.h
+++ shell_header.h
@@ -1,6 +1,16 @@
 #ifndef SIMPLE_SHELL
 #define SIMPLE_SHELL
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main (int argc, char **argv, char **env);
 
 
 #endif
