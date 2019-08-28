ca97d439cd3de7a54cf2d7db046dab72b9d29dfb
alejogonza <alejo.1996.2001@gmail.com>
2019-08-28 04:54:40 +0000

    [feature] works /bin/ls

---
+++ looper.c
@@ -0,0 +1,50 @@
+#include "shell_header.h"
+
+/**
+ * loop_shell - main function
+ * @b: string
+ * Return: Always 0.
+ */
+int loop_shell(void)
+{
+	char **arguments, *buffer, *getp, **argpath;
+	int execute, i = 0, j = 0;
+	size_t bufsize = TOK_BUFSIZE, num_size;
+	extern char **environ;
+	do
+	{
+		write (1, "$ ", 2);
+		buffer = malloc(bufsize * sizeof(char));
+		if (buffer == NULL)
+		{
+			free(buffer);
+			perror("Unable to allocate buffer");
+			return ('\0');
+		}
+		num_size = getline(&buffer, &bufsize, stdin);
+		buffer[num_size - 1] = '\0';
+		arguments = split_line(buffer);
+		getp = getpath("PATH=");
+		argpath = malloc(bufsize * sizeof(char));
+		argpath = split_line_path(getp);
+		while (argpath[i] != '\0')
+		{
+			while(argpath[i][j] != '\0')
+				j++;
+			if (argpath[i][j] == '\0')
+			{
+				argpath[i][j] = '/';
+				j++;
+				argpath[i][j] = '\0';
+			}
+			_strcat(argpath[i], buffer);
+			execve(arguments[0],arguments,environ);
+			i = i + 2;
+			j = 0;
+		}
+/*		execute = execute_arguments(arguments, num_size);*/
+		free(buffer);
+		free(arguments);
+	} while (execute);
+	return(0);
+}
---
+++ split.c
@@ -0,0 +1,102 @@
+#include "shell_header.h"
+
+/**
+ * *_realloc - prints buffer in hexa
+ * @ptr: pointer
+ * @old_size: size old
+ * @new_size: size new
+ * Return: ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return ('\0');
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
+
+/**
+ * split_line - function to split the line with tokens
+ * @buffer: Line of arguments
+ * Return: tokens
+ */
+
+char **split_line (char *buffer)
+{
+	char *token, **tokens, **back_tokens;
+	int posicion = 0, oldsize, newsize, bufsize = TOK_BUFSIZE;
+
+	tokens = malloc(TOK_BUFSIZE * sizeof(char*));
+	if (tokens == NULL)
+	{
+		perror("Error creating malloc");
+		exit(98);
+	}
+	token = strtok(buffer, " ");
+	while (token != NULL)
+	{
+		tokens[posicion] = token;
+		posicion++;
+		if (posicion >= bufsize)
+		{
+		bufsize += TOK_BUFSIZE;
+		back_tokens = tokens;
+		newsize = bufsize * sizeof(char*);
+		oldsize = (bufsize - TOK_BUFSIZE) * sizeof(char*);
+		tokens = _realloc(tokens, oldsize, newsize);
+		if (!tokens)
+			{
+				free(back_tokens);
+				perror("Error in realloc");
+				exit (98);
+			}
+		}
+		token = strtok(NULL, TOK_DELIM);
+	}
+	tokens[posicion] = NULL;
+	return (tokens);
+}
+
+
+/**
+ * split_line_path - function to split the line with tokens
+ * @buffer: Line of arguments
+ * Return: tokens
+ */
+
+char **split_line_path (char *buffer)
+{
+	char *token, **tokens, **back_tokens, *equal;
+	int posicion = 0, oldsize, newsize, bufsize = TOK_BUFSIZE;
+
+	tokens = malloc(TOK_BUFSIZE * sizeof(char*));
+	if (tokens == NULL)
+	{
+		perror("Error creating malloc");
+		return NULL;
+	}
+	equal = strtok(buffer, "=");
+	equal = strtok(NULL, "=");
+	token = strtok(equal, ":");
+	while (token != NULL)
+	{
+		tokens[posicion] = malloc((TOK_BUFSIZE * sizeof(char*)));
+		tokens[posicion] = token;
+		posicion++;
+		token = strtok(NULL, ":");
+	}
+	tokens[posicion] = NULL;
+	return (tokens);
+}
